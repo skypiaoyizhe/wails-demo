0c80bd3eb77999a8a4325a1866b96c6ec608b7fd
飘逸者 <529063865@qq.com>
2022-07-15 13:13:59 +0800

    [新增功能](master): init

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import {Test} from '../wailsjs/go/main/App'
+const handleTest = () => {
+  console.log("点击测试")
+  Test().then(res => {
+    console.log("结果",res)
+  })
+}
+</script>
+
+<template>
+  <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png"/>
+  <button @click="handleTest">点击测试</button>
+  <HelloWorld/>
+</template>
+
+<style>
+#logo {
+  display: block;
+  width: 50%;
+  height: 50%;
+  margin: auto;
+  padding: 10% 0 0;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-origin: content-box;
+}
+</style>
